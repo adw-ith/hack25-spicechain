69eed07ca781bce52410140a9f38b78832372f38
BifinJ <jamesbifin@gmail.com>
2025-08-24 00:04:00 +0530

    updated some farmer page and consumer page

--- frontend/src/app/(pages)/login/page.tsx
+++ frontend/src/app/(pages)/login/page.tsx
@@ -15,7 +15,7 @@ export default function LoginPage() {
 
     try {
       const endpoint = activeTab === "login" ? "/login" : "/signup";
-      const res = await fetch(`http://127.0.0.1:5000${endpoint}`, {
+      const res = await fetch(`http://127.0.0.1:5000/api${endpoint}`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(
